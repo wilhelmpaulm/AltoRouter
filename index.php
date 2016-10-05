a4279e8849a3b9b6878cfce4299b8246ebbb50af
Wilhelm Paul Martinez <wilhelmpaulm@gmail.com>
2016-10-05 12:45:51 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+$router = new AltoRouter();
+$router->setBasePath('');
+
+// put stuff here
+
+
+$match = $router->match();
+
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else if ($match) {
+    $target = $match["target"];
+    if (strpos($target, "#") !== false) {
+        list($controller, $action) = explode("#", $target);
+        $controller = new $controller;
+        $controller->$action($match["params"]);
+    } else {
+        require $match["target"];
+    }
+} else {
+    header("HTTP/1.0 404 Not Found");
+    response(null, 404);
+}
+  
